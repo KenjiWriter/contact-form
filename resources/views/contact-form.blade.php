8894d82494e9628204f170e15a107b415bc43c10
Maciek Wisniewski <m.wisniewski@elpromaelectronics.com>
2021-07-17 23:12:28 +0200

    Feat: Created form

---
+++ resources/views/contact-form.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Contact form by Wenzzi</title>
+</head>
+<body>
+    <fieldset>
+        <legend>Contact form</legend>
+        <div align='left'>
+            <form action="{{ route('post') }}">
+                <table>
+                    <tr>
+                        <td><label for="">*Your e-mail:</label> <input type="email" id="email" name="email" placeholder="mail@domian.com"></td>
+                    </tr>
+                    <tr>
+                        <td><label for="">(optional) First name:</label> <input type="email" id="email" name="email" placeholder="mail@domian.com"></td>
+                        <td><label for="">(optional) Last name:</label> <input type="email" id="email" name="email" placeholder="mail@domian.com"></td>
+                    </tr>
+                    <tr>
+                        <td><label for="">Priority:</label>
+                            <select name="priority" id="priority">
+                                <option value="1">Low</option>
+                                <option value="2">Medium</option>
+                                <option value="3">High</option>
+                            </select>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td><label for="">Message:</label></td>
+                    </tr>
+                    <tr>
+                        <td><textarea name="message" id="message" cols="30" rows="10"></textarea></td>
+                    </tr>
+                    <tr>
+                    <td><input align="center" type="submit" value="Send"></td>
+                    </tr>
+                <table>
+            </form>
+        </div>
+    </fieldset>
+</body>
+</html>
